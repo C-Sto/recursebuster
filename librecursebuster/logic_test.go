fa57e363868633323f461c79fe1b1820a476144e
c_sto <7466346+C-Sto@users.noreply.github.com>
2018-10-02 21:50:04 +0800

    start test files

---
+++ librecursebuster/logic_test.go
@@ -0,0 +1,21 @@
+package librecursebuster
+
+import (
+	"testing"
+)
+
+func TestManageRequests(t *testing.T) {
+	//todo: write test
+}
+
+func TestManageNewURLs(t *testing.T) {
+	//todo: write test
+}
+
+func TestTestURL(t *testing.T) {
+	//todo: write test
+}
+
+func TestDirBust(t *testing.T) {
+	//todo:write test
+}
